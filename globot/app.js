1b21df431b24dde26b5f4a88d9337071a270835e
parthwebdev <parth.webdev@gmail.com>
2026-01-28 12:19:51 +0530

    configure mongodb for vercel

--- globot/app.js
+++ globot/app.js
@@ -5,6 +5,9 @@ import adminRoutes from "./modules/admin/admin.routes.js";
 import studentRoutes from "./modules/user/student/student.route.js";
 import mailRoutes from "./modules/mail/mail.route.js";
 import consultationRoutes from "./modules/consultation/consultation.routes.js";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
 const app = express();
 
 const allowedOrigins = [
@@ -27,6 +30,26 @@ app.use(
 );
 app.use(express.json());
 
+// Connect MongoDB
+console.log("MONGODB_URI", process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+export const connectMongo = async () => {
+  try {
+    await client.connect();
+    console.log("mongodb is up");
+  } catch (error) {
+    console.log("mongodb is down", error);
+    process.exit(1);
+  }
+};
+connectMongo();
+
 //health check route
 app.get("/", (req, res) => {
   res.json({
@@ -47,6 +70,12 @@ app.use("/api/student", studentRoutes);
 app.use("/api/mail", mailRoutes);
 
 app.use("/api/consultation", consultationRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`GlobeBot server running on port ${PORT}`);
+});
+
 export default app;
 
 //payment routes;
