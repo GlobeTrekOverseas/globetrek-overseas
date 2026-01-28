53e92f836fc8973ae6d07c749b3f728d389abb6c
parthwebdev <parth.webdev@gmail.com>
2026-01-28 12:33:25 +0530

    use mongoose instead of native mongodb driver

--- globot/app.js
+++ globot/app.js
@@ -5,7 +5,8 @@ import adminRoutes from "./modules/admin/admin.routes.js";
 import studentRoutes from "./modules/user/student/student.route.js";
 import mailRoutes from "./modules/mail/mail.route.js";
 import consultationRoutes from "./modules/consultation/consultation.routes.js";
-import { MongoClient, ServerApiVersion } from "mongodb";
+// import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
@@ -31,18 +32,20 @@ app.use(
 app.use(express.json());
 
 // Connect MongoDB
+// const client = new MongoClient(process.env.MONGODB_URI, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 console.log("MONGODB_URI", process.env.MONGODB_URI);
-const client = new MongoClient(process.env.MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
 export const connectMongo = async () => {
   try {
-    await client.connect();
-    console.log("mongodb is up");
+    // await client.connect();
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const isConnected = conn.connections[0].readyState === 1;
+    console.log("MongoDB connected :", isConnected);
   } catch (error) {
     console.log("mongodb is down", error);
     process.exit(1);
